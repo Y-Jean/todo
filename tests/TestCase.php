fda6c16db765a32711d2e5b3e6c48e57261a6804
JSoo <jisoo2718@gmail.com>
2022-12-04 16:49:01 +0900

    - user, register controller 관련 unit test 생성 - phpunit.xml 파일 수정

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected static $user = null;
+
+    public function getUser()
+    {
+        if (self::$user === null) {
+            self::$user = $this->post('api/v1/login', [
+                'email' => 'test@example.com',
+                'password' => 'todo1234!!',
+            ])
+                ->original;
+
+            self::$user['token'] = ['Authorization' => self::$user['token_type']. ' ' . self::$user['token']];
+        }
+
+        return self::$user;
+    }
 }
