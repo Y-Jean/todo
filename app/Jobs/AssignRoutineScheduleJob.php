5eb0c07021a3508ece7d18a24f6dcb11d1fdf4fe
JSoo <jisoo2718@gmail.com>
2022-12-13 14:07:05 +0900

    fix psr12

--- app/Jobs/AssignRoutineScheduleJob.php
+++ app/Jobs/AssignRoutineScheduleJob.php
@@ -13,7 +13,10 @@ use Illuminate\Queue\SerializesModels;
 
 class AssignRoutineScheduleJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     /**
      * Create a new job instance.
@@ -35,23 +38,23 @@ class AssignRoutineScheduleJob implements ShouldQueue
         $now = Carbon::now()->startOfDay();
 
         $routines = Routine::with('tag')
-                            ->whereDate('start_date', '<=', $now->toDateString())
-                            ->where(function ($query) use ($now) {
-                                $query
-                                    ->whereDate('end_date', '>=', $now->toDateString())
-                                    ->orWhere('end_date', null);
-                            })
-                            ->where(function ($query) use ($now) {
-                                $query
-                                    ->where('type', 'month')
-                                    ->whereJsonContains('schedules->dates', $now->day);
-                            })
-                            ->orWhere(function ($query) use ($now) {
-                                $query
-                                    ->where('type', 'week')
-                                    ->whereJsonContains('schedules->days_of_week', strtolower($now->shortEnglishDayOfWeek));
-                            })
-                            ->get();
+                        ->whereDate('start_date', '<=', $now->toDateString())
+                        ->where(function ($query) use ($now) {
+                            $query
+                                ->whereDate('end_date', '>=', $now->toDateString())
+                                ->orWhere('end_date', null);
+                        })
+                        ->where(function ($query) use ($now) {
+                            $query
+                                ->where('type', 'month')
+                                ->whereJsonContains('schedules->dates', $now->day);
+                        })
+                        ->orWhere(function ($query) use ($now) {
+                            $query
+                                ->where('type', 'week')
+                                ->whereJsonContains('schedules->days_of_week', strtolower($now->shortEnglishDayOfWeek));
+                        })
+                        ->get();
 
         $routines->each(function ($routine) use ($now) {
             $task = new Task();
